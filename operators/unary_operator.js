aab9ef4b66c1f14bc571a037aeca2dffa83b49a5
Gourab Mahapatra <82425081+hardRockWorld@users.noreply.github.com>
2022-04-07 14:58:30 +0530

    increement + decreement operator

---
+++ operators/unary_operator.js
@@ -0,0 +1,13 @@
+// unary operator - increement operator
+let x = 0;
+console.log(x++);
+console.log(x);
+console.log(++x);
+console.log(x);
+
+// unary operator - decreement operator
+let y = 5;
+console.log(y--);
+console.log(y);
+console.log(--y);
+console.log(y);
